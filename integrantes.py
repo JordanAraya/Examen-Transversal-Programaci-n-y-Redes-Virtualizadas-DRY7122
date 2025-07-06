356d1c30a6b4d5789bf848fe71455bfde400428a
NetAcad DEVASC <changeme@example.com>
2025-07-06 22:41:07 +0000

    Agrega script para listar integrantes

---
+++ integrantes.py
@@ -0,0 +1,11 @@
+# integrantes.py
+def main():
+    integrantes = [
+        "Jordan Araya",
+        "Falon Zambrano"
+    ]
+    for persona in integrantes:
+        print(persona)
+
+if __name__ == "__main__":
+    main()
